2871ca5069121702e7217ca0d60a90438c1a8e99
katiedonkers <36115268+katiedonkers@users.noreply.github.com>
2018-02-06 15:22:53 -0500

    Test push

--- src/sysc3303/group/code/SimpleEchoServer.java
+++ src/sysc3303/group/code/SimpleEchoServer.java
@@ -20,6 +20,7 @@ public class SimpleEchoServer {
    {
       try {
          receiveSocket = new DatagramSocket(6969);
+         //Katie test
          
       } catch (SocketException se) {
          se.printStackTrace();
